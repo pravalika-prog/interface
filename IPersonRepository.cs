593723bd8aa9989b65ef37898266f326ad0fe990
pravalika-prog <68769158+pravalika-prog@users.noreply.github.com>
2020-12-07 19:44:09 +0530

    Add files via upload

--- IPersonRepository.cs
+++ IPersonRepository.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace PersonRepository.Interface
 {
-    interface IPersonRepository
+   public interface IPersonRepository
     {
+        IEnumerable<Person> GetPeople();
+
+        Person GetPerson(int id);
+
+        void AddPerson(Person newPerson);
+
+        void UpdatePerson(int id, Person updatedPerson);
+
+        void DeletePerson(int id);
     }
 }
