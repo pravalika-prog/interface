e56164ed7f6e06a274cb3a7ec9356e0bc7ee81b2
pravalika-prog <68769158+pravalika-prog@users.noreply.github.com>
2020-12-08 18:51:06 +0530

    Add files via upload

---
+++ Vehicle.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    interface Vehicle
+    {
+        void changeGear(int a);
+        void speedUp(int a);
+        void applyBrakes(int a);
+    }
+
+
+    class Bicycle : Vehicle
+    {
+
+        int speed;
+        int gear;
+
+
+        public void changeGear(int newGear)
+        {
+
+            gear = newGear;
+        }
+
+
+        public void speedUp(int increment)
+        {
+
+            speed = speed + increment;
+        }
+        public void applyBrakes(int decrement)
+        {
+
+            speed = speed - decrement;
+        }
+
+        public void printStates()
+        {
+            Console.WriteLine("speed: " + speed +
+                              " gear: " + gear);
+        }
+        class Bike : Vehicle
+        {
+
+            int speed;
+            int gear;
+
+
+            public void changeGear(int newGear)
+            {
+
+                gear = newGear;
+            }
+
+
+            public void speedUp(int increment)
+            {
+
+                speed = speed + increment;
+            }
+
+
+            public void applyBrakes(int decrement)
+            {
+                speed = speed - decrement;
+            }
+
+            public void printStates()
+            {
+                Console.WriteLine("speed: " + speed +
+                                  " gear: " + gear);
+            }
+            class GFG
+            {
+
+
+                public static void Main(String[] args)
+                {
+
+
+                    Bicycle bicycle = new Bicycle();
+                    bicycle.changeGear(2);
+                    bicycle.speedUp(3);
+                    bicycle.applyBrakes(1);
+
+                    Console.WriteLine("Bicycle present state :");
+                    bicycle.printStates();
+
+
+                    Bike bike = new Bike();
+                    bike.changeGear(1);
+                    bike.speedUp(4);
+                    bike.applyBrakes(3);
+
+                    Console.WriteLine("Bike present state :");
+                    bike.printStates();
+
+                }
+
+            }
+
+        }
+    }
+}
---
+++ inter1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    interface inter1
+    {
+        void display();
+    }
+
+
+    class testClass : inter1
+    {
+
+
+        public void display()
+        {
+            Console.WriteLine("Sudo Placement GeeksforGeeks");
+        }
+
+        
+        public static void Main(String[] args)
+        {
+            testClass t = new testClass();
+
+            // calling method 
+            t.display();
+        }
+    }
+}
+       
