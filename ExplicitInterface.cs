3fba8199cfcfdaf849c5f836105ca029cd63eaa4
pravalika-prog <68769158+pravalika-prog@users.noreply.github.com>
2020-12-09 18:12:27 +0530

    Add files via upload

---
+++ ExplicitInterface.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    interface ExplicitInterface
+    {
+        void print();
+    }
+    interface i2
+    {
+        void print();
+    }
+    class gfh: ExplicitInterface,i2
+    {
+        
+
+        void ExplicitInterface.print()
+        {
+            Console.WriteLine("present");
+        }
+        void i2.print()
+        {
+            Console.WriteLine("people");
+        }
+    }
+
+     static void Main(string[] args)
+    {
+       ExplicitInterface  i1 = new gfh();
+        i2 h = new gfh();
+        i1.print();
+
+
+        h.print();
+    }
+}
