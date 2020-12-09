3497b19f6f48f9d92fdcab645de8fc4930f79045
pravalika-prog <68769158+pravalika-prog@users.noreply.github.com>
2020-12-09 18:15:13 +0530

    Add files via upload

---
+++ Iexample.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    interface Iexample
+    {
+        void add(int a, int b);
+    }
+    interface people : Iexample
+    {
+        void sub(int c, int d);
+
+    }
+    class buy : people
+    {
+      public  void add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+    public    void sub(int c, int d)
+        {
+            Console.WriteLine(c - d);  
+        }
+    
+    
+        static void Main()
+        {
+          buy b= new buy();
+            b.add(100, 200);
+            b.sub(100,
+                
+                
+                78);
+        }
+    }
+}
